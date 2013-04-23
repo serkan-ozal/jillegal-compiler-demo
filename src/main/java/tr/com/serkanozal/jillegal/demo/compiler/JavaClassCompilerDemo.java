72ce34a8b37bd4f3ad35be31356b82036ca5578c
unknown <Asus@Asus-Bilgisayar.(none)>
2013-04-23 22:02:29 +0300

    Initial commit

---
+++ src/main/java/tr/com/serkanozal/jillegal/demo/compiler/JavaClassCompilerDemo.java
@@ -0,0 +1,18 @@
+/**
+ * @author SERKAN OZAL
+ *         
+ *         E-Mail: <a href="mailto:serkanozal86@hotmail.com">serkanozal86@hotmail.com</a>
+ *         GitHub: <a>https://github.com/serkan-ozal</a>
+ */
+
+package tr.com.serkanozal.jillegal.demo.compiler;
+
+import tr.com.serkanozal.jillegal.core.Jillegal;
+
+public class JavaClassCompilerDemo {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		Jillegal.init();
+	}
+	
+}
