81a91406a9df5c002f8143580767ccb1d2d11da0
unknown <Asus@Asus-Bilgisayar.(none)>
2013-04-25 09:42:50 +0300

    demo implemented for jillegal-compiler-1.0.0-RELEASE

--- src/main/java/tr/com/serkanozal/jillegal/demo/compiler/GroovyClassCompilerDemo.java
+++ src/main/java/tr/com/serkanozal/jillegal/demo/compiler/GroovyClassCompilerDemo.java
@@ -7,12 +7,37 @@
 
 package tr.com.serkanozal.jillegal.demo.compiler;
 
+import tr.com.serkanozal.jillegal.compiler.domain.model.DefaultCodeType;
+import tr.com.serkanozal.jillegal.compiler.exception.ClassCompileException;
+import tr.com.serkanozal.jillegal.compiler.factory.ClassCompilerFactory;
+import tr.com.serkanozal.jillegal.compiler.factory.DefaultClassCompilerFactory;
 import tr.com.serkanozal.jillegal.core.Jillegal;
 
 public class GroovyClassCompilerDemo {
 
-	public static void main(String[] args) {
+	static {
 		Jillegal.init();
+	}
+	
+	public static void main(String[] args) throws ClassCompileException, InstantiationException, IllegalAccessException {
+		ClassCompilerFactory classCompilerFactory = new DefaultClassCompilerFactory();
+		
+		String code = 
+				"package tr.com.serkanozal.jillegal.compiler;"+ "\n" +
+				"\n" +
+				"public class SampleGroovyClass {" + "\n" +
+				"\n" +
+				"\t" + "public String toString() {" + "\n" +
+				"\t" + "\t" + "def str = \"I am SampleGroovyClass\";" + "\n" +
+				"\t" + "\t" + "return str;" + "\n" +
+				"\t" + 	"}" + "\n" +
+				"\n" +
+				"}";
+
+		Class<?> compiledClass = classCompilerFactory.getClassCompiler(DefaultCodeType.GROOVY).compile(code);
+		System.out.println("In Memory Compiled Groovy Class: " + compiledClass.getName());
+		Object obj = compiledClass.newInstance();
+		System.out.println(obj.toString());
 	}
 	
 }
--- src/main/java/tr/com/serkanozal/jillegal/demo/compiler/JavaClassCompilerDemo.java
+++ src/main/java/tr/com/serkanozal/jillegal/demo/compiler/JavaClassCompilerDemo.java
@@ -7,12 +7,36 @@
 
 package tr.com.serkanozal.jillegal.demo.compiler;
 
+import tr.com.serkanozal.jillegal.compiler.domain.model.DefaultCodeType;
+import tr.com.serkanozal.jillegal.compiler.exception.ClassCompileException;
+import tr.com.serkanozal.jillegal.compiler.factory.ClassCompilerFactory;
+import tr.com.serkanozal.jillegal.compiler.factory.DefaultClassCompilerFactory;
 import tr.com.serkanozal.jillegal.core.Jillegal;
 
 public class JavaClassCompilerDemo {
 
-	public static void main(String[] args) throws ClassNotFoundException {
+	static {
 		Jillegal.init();
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, ClassCompileException, InstantiationException, IllegalAccessException {
+		ClassCompilerFactory classCompilerFactory = new DefaultClassCompilerFactory();
+		
+		String code = 
+				"package tr.com.serkanozal.jillegal.compiler;"+ "\n" +
+				"\n" +
+				"public class SampleJavaClass {" + "\n" +
+				"\n" +
+				"\t" + "public String toString() {" + "\n" +
+				"\t" + "\t" + "return \"I am SampleJavaClass\";" + "\n" +
+				"\t" + 	"}" + "\n" +
+				"\n" +
+				"}";
+
+		Class<?> compiledClass = classCompilerFactory.getClassCompiler(DefaultCodeType.JAVA).compile(code);
+		System.out.println("In Memory Compiled Java Class: " + compiledClass.getName());
+		Object obj = compiledClass.newInstance();
+		System.out.println(obj.toString());
 	}
 	
 }
